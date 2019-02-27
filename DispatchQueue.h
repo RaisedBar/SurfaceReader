19d64f9e468e9fe2bd9f59fe84d485c27bd2df18
timburg1 <tim@raisedbar.net>
2019-02-27 17:50:31 +0000

    Changed the DispatchQueue class header-only (DispatchQueue.h)

--- DispatchQueue.h
+++ DispatchQueue.h
@@ -15,14 +15,63 @@ class dispatch_queue
 	typedef std::function<void(void)> fp_t;
 
 public:
-	dispatch_queue(size_t thread_cnt = 1);
-	~dispatch_queue();
+	dispatch_queue(size_t thread_cnt = 1)
+	{
+		std::vector<std::thread>::iterator it;
+
+		for (it = threads_.begin(); it != threads_.end(); it++)
+		{
+			*it = std::thread(&dispatch_queue::dispatch_thread_handler, this);
+		}
+	};
+	
+	~dispatch_queue()
+	{
+		// printf("Destructor: Destroying dispatch threads...\n");
+
+		// Signal to dispatch threads that it's time to wrap up 
+		std::unique_lock<std::mutex> lock(lock_);
+		quit_ = true;
+		lock.unlock();
+		cv_.notify_all();
+
+		// Wait for threads to finish before we exit 
+		std::vector<std::thread>::iterator it;
+
+		for (it = threads_.begin(); it != threads_.end(); it++)
+		{
+			if (it->joinable())
+			{
+				it->join();
+			}
+		}
+	};
 
 	// dispatch and copy 
-	void dispatch(const fp_t& op);
+	void dispatch(const fp_t& op)
+	{
+		std::unique_lock<std::mutex> lock(lock_);
+		q_.push(op);
+
+		// Manual unlocking is done before notifying, to avoid waking up 
+	// the waiting thread only to block again (see notify_one for details) 
+		lock.unlock();
+		cv_.notify_all();
+	};
+	
 	// dispatch and move 
-	void dispatch(fp_t&& op);
-		// Deleted operations 
+	void dispatch(fp_t&& op)
+	{
+		std::unique_lock<std::mutex> lock(lock_);
+		q_.push(std::move(op));
+
+		// Manual unlocking is done before notifying, to avoid waking up 
+	// the waiting thread only to block again (see notify_one for details) 
+		lock.unlock();
+		cv_.notify_all();
+	};
+		
+	// Deleted operations 
 	dispatch_queue(const dispatch_queue& rhs) = delete;
 	dispatch_queue& operator=(const dispatch_queue& rhs) = delete;
 	dispatch_queue(dispatch_queue&& rhs) = delete;
@@ -34,5 +83,28 @@ private:
 	std::queue<fp_t> q_;
 	std::condition_variable cv_;
 	bool quit_ = false;
-	void dispatch_thread_handler(void);
+	void dispatch_thread_handler(void)
+	{
+		std::unique_lock<std::mutex> lock(lock_);
+
+		do
+		{
+			//Wait until we have data or a quit signal 
+			cv_.wait(lock, [this] {
+				return (q_.size() || quit_);
+			});
+
+			//after wait, we own the lock 
+			if (!quit_ && q_.size())
+			{
+				auto op = std::move(q_.front());
+				q_.pop();
+
+				//unlock now that we're done messing with the queue 
+				lock.unlock();
+				op();
+				lock.lock();
+			}
+		} while (!quit_);
+	};
 };
