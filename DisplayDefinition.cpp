63815a8fae1b2f22b8c0fe32866c32b9f37ca609
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2021-05-13 02:54:33 +0100

    Update the DisplayDefinition class to use auto to define variables and starts_with when testing strings.

--- DisplayDefinition.cpp
+++ DisplayDefinition.cpp
@@ -20,7 +20,6 @@ strLinkedDisplayHash( strEmpty),
 strTranslationID( strEmpty)
 {}
 
-
 DisplayDefinition::DisplayDefinition( bool blnIsLED)
 		:
 MessageDefinition(),
@@ -36,129 +35,107 @@ strLinkedDisplayHash( strEmpty),
 strTranslationID( strEmpty)
 {}
 
-
 DisplayDefinition::~DisplayDefinition()
 	{}
-
 
 bool DisplayDefinition::IsLEDLamp()
 {
 	return blnIsLEDLamp;
 }
 
-
 void DisplayDefinition::IsLEDLamp( bool blnIsLED)
 {
 	blnIsLEDLamp = blnIsLED;
 }
-
 
 	int DisplayDefinition::GetLineCount()
 {
 return nDisplayLineCount;
 }
 
-
 void DisplayDefinition::SetLineCount( int nLines)
 {
 nDisplayLineCount = nLines;
 }
-
 
 int DisplayDefinition::GetDisplayLength()
 {
 return nDisplayLength;
 }
 
-
 void DisplayDefinition::SetDisplayLength( int nLength)
 {
 nDisplayLength = nLength;
 }
-
 
 int DisplayDefinition::GetStripCount()
 {
 return nStripCount;
 }
 
-
 void DisplayDefinition::SetStripCount( int nStrips)
 {
 nStripCount= nStrips;
 }
-
 
 std::string DisplayDefinition::GetCharacterTranslationID()
 {
 return strTranslationID;
 }
 
-
 void DisplayDefinition::SetCharacterTranslationID( std::string strTableID)
 {
 strTranslationID = strTableID;
 }
-
 
 int DisplayDefinition::GetSysExCursorOffset()
 {
 	return nCursorOffset;
 }
 
-
 void DisplayDefinition::SetSysExCursorOffset( int nNewCursorOffset)
 {
 	nCursorOffset = nNewCursorOffset;
 }
 
-
 int DisplayDefinition::GetNibbledCursorPosition( std::string strHash, unsigned char nData2)
 {
-int nResult = -1;
+auto nResult = -1;
 
-if (strHash.substr( 0, 1).compare( strNibbleHashPrefix) == 0)
+if (strHash.starts_with( strNibbleHashPrefix))
 {
-nResult = nData2 & MIDI_CHN_MASK;;
+nResult = nData2 & MIDI_CHN_MASK;
 }
 
 return nResult;
 }
-
 
 bool DisplayDefinition::IsCursorFromLeft()
 {
 	return blnCursorFromLeft;
 }
 
-
 void DisplayDefinition::SetCursorFromLeft( bool blnLefty)
 {
 	blnCursorFromLeft = blnLefty;
 }
-
 
 bool DisplayDefinition::IsUniCode()
 {
 return blnIsUniCode;
 }
 
-
 void DisplayDefinition::SetUniCode( bool blnUniCoded)
 {
 	blnIsUniCode = blnUniCoded;
 }
-
 
 std::string DisplayDefinition::GetLinkedDisplayHash()
 {
 return strLinkedDisplayHash;
 }
 
-
 void DisplayDefinition::SetLinkedDisplayHash( std::string strLinkedHash)
 {
 strLinkedDisplayHash = strLinkedHash;
 }
-
-
